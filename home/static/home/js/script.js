20b51e2eb318ff47db53b33063aee7fafd9f767b
João Vitor Moreira Passos <moreirapassosj@gmail.com>
2023-02-03 21:32:06 -0300

    adicionando botões para mover o carrossel de noticías

--- home/static/home/js/script.js
+++ home/static/home/js/script.js
@@ -1,20 +1,23 @@
-document.getElementById("move-scroll-right").addEventListener("click", () => {
-    let scroll = document.getElementById("cards-container");
-    if (scroll.scrollWidth > scroll.scrollLeft + scroll.clientWidth) {
-        document.getElementById("cards-container").scrollTo(scroll.scrollLeft + 800, 0);
-        // document.getElementById("cards-container").scrollLeft += 500;
-    }
-    else{
-        document.getElementById("cards-container").scrollTo(scrollWidth - 800);
-    }
-})
+try{
+    document.getElementById("move-scroll-right").addEventListener("click", () => {
+        let scroll = document.getElementById("cards-container");
+        if (scroll.scrollWidth > scroll.scrollLeft + scroll.clientWidth) {
+            document.getElementById("cards-container").scrollTo(scroll.scrollLeft + 800, 0);
+        }
+        else{
+            document.getElementById("cards-container").scrollTo(scrollWidth - 800);
+        }
+    })
+}catch{}
 
-document.getElementById("move-scroll-left").addEventListener("click", () => {
-    let scroll = document.getElementById("cards-container");
-    if (scroll.scrollLeft > 0) {
-        document.getElementById("cards-container").scrollLeft -= 800;
-    }
-    else{
-        document.getElementById("cards-container").scrollLeft = 0;
-    }
-})
+try{
+    document.getElementById("move-scroll-left").addEventListener("click", () => {
+        let scroll = document.getElementById("cards-container");
+        if (scroll.scrollLeft > 0) {
+            document.getElementById("cards-container").scrollLeft -= 800;
+        }
+        else{
+            document.getElementById("cards-container").scrollLeft = 0;
+        }
+    })
+}catch{}
