dbb0460bac922dfd5baedc2bd9f2a516b350f8ba
João Vitor Moreira Passos <moreirapassosj@gmail.com>
2023-02-21 03:43:26 -0300

    Adicionando Fotos e Nomes dos participantes do projeto e dividindo os cards quando ficam um em cima do outro

--- sobre/views.py
+++ sobre/views.py
@@ -4,4 +4,21 @@ from django.shortcuts import render
 
 
 def sobre(request):
-    return render(request, 'sobre/page/sobre.html')
+    devs = [
+        {
+            "way": "sobre/img/Ellipse 3.png",
+            "nome": "João Vitor Moreira Passos"
+        },
+        {
+            "way": "sobre/img/Ellipse 4.png",
+            "nome": "João Victor de Lima "
+        },
+        {
+            "way": "sobre/img/Ellipse 5.png",
+            "nome": "Dayan Ramos Gomes"
+        },
+    ]
+    devs[-1]["last"] = True
+    return render(request, 'sobre/page/sobre.html', context={
+        "devs": devs,
+    })
