b8b93716f207452c4d3903827f61b817feecfc29
João Vitor Moreira Passos <moreirapassosj@gmail.com>
2023-03-30 10:53:19 -0300

    finalizando, por enquanto

--- home/views.py
+++ home/views.py
@@ -4,4 +4,38 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'home/page/home.html')
+
+    noticias = [
+        {
+            'titulo': 'Nutrição UFPI',
+            'imagem': 'home/img/image 2.png',
+            'descricao': '''Curso de Nutrição abre campanha para ajudar os
+estudantes a se alimentaram melhor no seu dia a dia.''',
+            'tempo': 'Postado há 16 horas',
+            'data': '10/01/2023',
+            'last': False
+        },
+        {
+            'titulo': 'Sistemas de Informação UFPI',
+            'imagem': 'home/img/image 1.png',
+            'descricao': '''Professores do curso de Sistemas de Informação,
+abrem vagas com bolsas para projetos de iniciação Científica e Tecnológica.''',
+            'tempo': 'Postado há 2 dias',
+            'data': '08/01/2023',
+            'last': False
+        },
+        {
+            'titulo': 'Medicina UFPI',
+            'imagem': 'home/img/image 3.png',
+            'descricao': '''Curso de medicina abre 20 vagas de estágio em
+hospitais e postos de saúde para os alunos poderem vivenciar na prática seus
+aprendizados.''',
+            'tempo': 'Postado há 6 dias',
+            'data': '04/01/2023',
+            'last': True
+        }
+    ]
+
+    return render(request, 'home/page/home.html', context={
+        'noticias': noticias
+    })
--- nae/views.py
+++ nae/views.py
@@ -4,4 +4,41 @@ from django.shortcuts import render
 
 
 def nae(request):
-    return render(request, 'nae/page/nae.html')
+
+    noticias = [
+        {
+            'titulo': 'Abertura EditalBolsa BAE',
+            'imagem': 'nae/img/image 2.png',
+            'descricao': '''Bolsa de Auxílio Estudantil (BAE),
+foi aberta no ano de 2023 para que novos alunos possam concorrer
+a bolsa no valor de 400 reais.''',
+            'tempo': 'Publicado há 2 dias',
+            'data': '09/01/2023',
+            'last': False
+        },
+        {
+            'titulo': 'Segunda Chamada Auxílio Residencial',
+            'imagem': 'nae/img/image 2.png',
+            'descricao': '''Mais 20 alunos alunos da lista de espera do auxílio
+residencia foram chamados nessa Quarta feira para receberem a bolsa de 400 
+reais.''',
+            'tempo': 'Publicado há 3 horas',
+            'data': '11/01/2023',
+            'last': False
+        },
+        {
+            'titulo': 'NAE promove evento musical',
+            'imagem': 'nae/img/image 2.png',
+            'descricao': '''O NAE em conjunto com alunos de vários cursos da 
+universidade, realizaram um evento musical no Restaurante Universitáio nesta
+sexta feira.''',
+            'tempo': 'Publicado há 5 dias',
+            'data': '06/01/2023',
+            'last': False
+        }
+
+    ]
+
+    return render(request, 'nae/page/nae.html', context={
+        'noticias': noticias
+    })
