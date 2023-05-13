833ae055c0b1d28760adad14937299bd209fe687
Jo??o Vitor Moreira Passos <moreirapassosj@gmail.com>
2023-05-12 22:10:36 -0300

    Reestruturando o projeto

--- contato/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-
-def contato(request):
-    return render(request, 'contato/page/contato.html')
--- nae/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('NAE/', views.nae, name='nae')
-]
