3cef6822e55eb3a8418f14736b5e741db2325d53
Jo??o Vitor Moreira Passos <moreirapassosj@gmail.com>
2023-06-07 12:02:32 -0300

    escopo para os  models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Profile, Category, Notice, Comment, Recipe, Ingredient
 
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Category)
+admin.site.register(Notice)
+admin.site.register(Comment)
+admin.site.register(Recipe)
+admin.site.register(Ingredient)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="profile/", blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    birth_date = models.DateField(blank=True, null=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    address = models.CharField(max_length=100, blank=True, null=True)
+    linkedin = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    image_pattern = models.ImageField("/category/", blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Notice(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    content = models.TextField()
+    date = models.DateField(auto_now_add=True)
+    date_time = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to="notice/", blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    notice = models.ForeignKey(Notice, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content[:10]
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="recipe/", blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    recipe = models.ManyToManyField(Recipe, related_name="ingredients")
+
+    def __str__(self):
+        return self.name
